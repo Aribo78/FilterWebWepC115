60a85c5bb4c3ae5e08d6e1e61a7fcdcf49d081d5
Aribo78 <89050884+Aribo78@users.noreply.github.com>
2023-11-03 20:31:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+noseX=0;
+noseY=0;
 function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/8zb6kbBj/Mammoth-Fruit.webp')
 }
-
 function setup(){
     canvas = createCanvas(300, 300);
     canvas.center();
@@ -11,25 +13,25 @@ function setup(){
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
-
 function modelLoaded() {
     console.log('PoseNet Is Initialized');
 }
-
 function gotPoses(results)  
 {
     if(results.length > 0)
     {
         console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
         console.log("nose x = " + results[0].pose.nose.x);
         console.log("nose y = " + results[0].pose.nose.y);
     }
 }
-
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 30, 30);
 }
-
 function take_snapshot(){
     save('myFilterImage.png');
 }
+
